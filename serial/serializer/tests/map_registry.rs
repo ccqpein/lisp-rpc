c0035bf62ed2e615d0857a1d77907743bc53007e
ccQpein <ccqpein@protonmail.com>
2026-05-06 23:37:55 -0400

    add the `deserialize_option` and fix the kebab case bug

--- serial/serializer/tests/map_registry.rs
+++ serial/serializer/tests/map_registry.rs
@@ -2,8 +2,29 @@ use lisp_rpc_rust_serializer::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct LanguagePerfer {
+    lang: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct GlobalLanguage {
     name: String,
+}
+
+#[test]
+fn test_local_map_registration() {
+    let mut buf = Vec::with_capacity(1024);
+    let mut s = LispRPCSerializer::new(&mut buf);
+
+    let lp = LanguagePerfer {
+        lang: "eng".to_string(),
+    };
+
+    s.register_map_type("LanguagePerfer");
+    lp.serialize(&mut s).unwrap();
+
+    let serialized = std::str::from_utf8(&s.output[..s.pos]).unwrap();
+    assert_eq!(serialized, r#"'(:lang "eng")"#);
 }
 
 #[test]
@@ -11,14 +32,12 @@ fn test_global_dispatch_serialization() {
     let mut buf = Vec::with_capacity(1024);
     let mut s = LispRPCSerializer::new(&mut buf);
 
-    // Register GlobalLanguage globally
     register_global_map_type("GlobalLanguage");
 
     let gl = GlobalLanguage {
         name: "rust".to_string(),
     };
 
-    // Serialize normally - it should pick up the map style from the GLOBAL registry
     gl.serialize(&mut s).unwrap();
 
     let serialized = std::str::from_utf8(&s.output[..s.pos]).unwrap();
@@ -36,7 +55,6 @@ fn test_global_dispatch_serialization() {
 fn test_clear_global_registry() {
     let mut buf = Vec::with_capacity(1024);
     
-    // 1. Register and verify it works
     register_global_map_type("GlobalLanguage");
     let gl = GlobalLanguage { name: "rust".to_string() };
     
@@ -44,11 +62,9 @@ fn test_clear_global_registry() {
     gl.serialize(&mut s1).unwrap();
     assert_eq!(std::str::from_utf8(&s1.output[..s1.pos]).unwrap(), r#"'(:name "rust")"#);
     
-    // 2. Clear and verify it reverts to Struct style
     clear_global_map_types();
     buf.clear();
     let mut s2 = LispRPCSerializer::new(&mut buf);
     gl.serialize(&mut s2).unwrap();
-    // Should now be back to (global-language :name "rust")
     assert_eq!(std::str::from_utf8(&s2.output[..s2.pos]).unwrap(), r#"(global-language :name "rust")"#);
 }
